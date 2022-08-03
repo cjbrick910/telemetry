49b57c00f59f890e5cb2a827b7d3462022f3233c
chris <chris@DESKTOP-IGA9MM9>
2022-08-03 15:53:22 -0400

    Add project files.

---
+++ base-station/App.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Renci.SshNet;
+using Renci.SshNet.Common;
+
+
+namespace base_station
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+       
+        public static string Connect(string password)
+        {
+            string host = "10.10.0.95";
+            string username = "lenin";
+
+            SshClient client = new SshClient(host, username, password);
+            client.Connect();
+
+            if(client.IsConnected)
+            {
+                return "Connected!";
+            }
+            else
+            {
+                return "Not Connected";
+            }
+            
+
+        }
+
+        public static string sendCommand(string command, string password)
+        {
+            string host = "10.10.0.95";
+            string username = "lenin";
+
+            SshClient client = new SshClient(host, username, password);
+            client.Connect();
+
+            var cmd = client.CreateCommand(command);
+
+            string output = cmd.Execute();
+
+            client.RunCommand(command);
+
+
+            
+
+            return output;
+        }
+    }
+}
---
+++ base-station/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace base_station
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            
+            
+            InitializeComponent();
+
+        }
+
+        private void sshLogin(object sender, RoutedEventArgs e)
+        {
+            string password = SSH_Password.Password.ToString();
+
+            status.Text = App.Connect(password);
+        }
+
+        private void sendCommand(object sender, RoutedEventArgs e)
+        {
+            string command = commandInput.Text.ToString();
+            string password = SSH_Password.Password.ToString();
+
+            string commandOutput = App.sendCommand(command, password);
+
+            output.Text = commandOutput;
+
+        }
+    }
+}
