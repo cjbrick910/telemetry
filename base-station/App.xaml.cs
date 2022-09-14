688bbeaa22ffdd1fbe4791c76a4341037804a3c5
dany <71732131+DanyPalma@users.noreply.github.com>
2022-09-13 20:57:44 -0400

    Update App.xaml.cs
    added switch case statement for both sensors

--- base-station/App.xaml.cs
+++ base-station/App.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Diagnostics;
@@ -67,14 +67,47 @@ namespace base_station
          * Creates new client and connects, then creates command and executes it
          * Returns a string with the data (It will not format the string automatically, that should be done on the other side)
          */
-        public static string readData(SshClient client)
+        public static string readData(SshClient client, string sensor )
         {
-            var command = client.CreateCommand("head -n 1 /dev/ttyUSB0 | cut -b 1-4");
-            string data = command.Execute();
             
-            return data;
-        }
+            switch(sensor)
+            {
+                case breakPressure:
 
+                var command = client.CreateCommand("head -n 1 /dev/tty/USB1 | cut -b 1-4");
+
+
+                string pressure = command.Execute();
+
+                return pressure; 
+
+                break; 
+
+                case rpm:
+
+                var command = client.createCommand("head -n 1 /dev/tty/USB0  | cut -b 1-3");
+                
+                string rotations = command.Execute();
+
+                return rotations; 
+
+
+                break; 
+
+                
+
+            }
+            
+            
+            
+
+
+            // var command = client.CreateCommand("head -n 1 /dev/ttyUSB0 | cut -b 1-4");
+    
+            // string data = command.Execute();
+            
+            // return data;
+        }
         //Other readData function that will create a connection each time it is called (not ideal because this is slower and can cause performance issues on the uplink)
 
         /*
